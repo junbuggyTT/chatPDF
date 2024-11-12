8978313cc55448dd27cde3d912a1906e06a7fecd
Junyoung Yoon <junyoung0654@gmail.com>
2024-11-12 14:17:26 -0800

    Persist chat logs to DB

--- chatpdf/src/components/ChatComponent.tsx
+++ chatpdf/src/components/ChatComponent.tsx
@@ -5,24 +5,51 @@ import { Message } from "ai";
 import { useChat } from "ai/react";
 import { Send } from "lucide-react";
 import MessageList from "./MessageList";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
 type Props = { chatId: number };
 
 const ChatComponent = ({ chatId }: Props) => {
+    const { data, isLoading } = useQuery({
+        queryKey: ["chat", chatId],
+        queryFn: async () => {
+            const response = await axios.post<Message[]>("/api/get-messages", {
+                chatId,
+            });
+            return response.data;
+        },
+    });
+
     const { input, handleInputChange, handleSubmit, messages } = useChat({
         api: "/api/chat",
+        body: {
+            chatId,
+        },
+        initialMessages: data || [],
     });
-    
+    React.useEffect(() => {
+        const messageContainer = document.getElementById("message-container");
+        if (messageContainer) {
+            messageContainer.scrollTo({
+                top: messageContainer.scrollHeight,
+                behavior: "smooth",
+            });
+        }
+    }, [messages]);
     return (
-        <div>
+        <div
+            className="relative max-h-screen overflow-scroll"
+            id="message-container"
+        >
             {/* header */}
             <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
                 <h3 className="text-xl font-bold">Chat</h3>
             </div>
 
             {/* message list */}
-            <MessageList messages={messages} />
+            <MessageList messages={messages} isLoading={isLoading} />
             <form
                 onSubmit={handleSubmit}
                 className="sticky bottom-0 inset-x-0 px-2 py-4 bg-white"
